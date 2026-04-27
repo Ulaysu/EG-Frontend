9fe572da43fb1267c0b9be6e64f6cfd039f8c856
Sulayman Sanyang <emiress921@gmail.com>
2026-04-27 08:23:12 +0000

    feat: add tourliist to list tours

---
+++ src/components/tours/TourList.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <TourCard v-for="tour in tours" :key="tour.id" :tour="tour" />
+  </section>
+</template>
+
+<script setup>
+import TourCard from './TourCard.vue'
+
+defineProps({
+  tours: {
+    type: Array,
+    required: true
+  }
+})
+</script>
